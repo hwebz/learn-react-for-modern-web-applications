0b4cdfb3e218dd6bcce6ac4eb3d7ea2053c8ff15
Do Manh Ha <ex-domanhha001@ocft.com>
2020-08-12 14:17:42 +0700

    Navbar routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { 
     HomePage,
     CounterButtonPage,
@@ -9,39 +9,45 @@ import {
     ControlledFormPage,
     UncontrolledFormPage
   } from './pages'
+import { NavBar } from './NavBar';
+import { FormsNavBar } from './FormsNavBar';
 
 function App() {
   return (
     <Router>
-      <Link to="/">Home</Link>
-      <Link to="/counter?startingValue=2">Counter</Link>
-      <Link to="/people-list">People List</Link>
-      <Link to="/protected">Protected Page</Link>
-      <Link to="/controlled">Controlled Form</Link>
-      <Link to="/uncontrolled">Uncontrolled Form</Link>
-      <Switch>
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
-        <Route path="/counter">
-          <CounterButtonPage />
-        </Route>
-        <Route path="/people-list">
-          <PeopleListPage />
-        </Route>
-        <Route path="/protected">
-          <ProtectedPage />
-        </Route>
-        <Route path="/controlled">
-          <ControlledFormPage />
-        </Route>
-        <Route path="/uncontrolled">
-          <UncontrolledFormPage />
-        </Route>
-        <Route>
-          <NotFoundPage />
-        </Route>
-      </Switch>
+      <NavBar />
+      <div className="App-header">
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/counter">
+            <CounterButtonPage />
+          </Route>
+          <Route path="/people-list">
+            <PeopleListPage />
+          </Route>
+          <Route path="/protected">
+            <ProtectedPage />
+          </Route>
+          <Route path="/forms">
+            <Router>
+              <FormsNavBar />
+              <Switch>
+                <Route path="/forms/controlled">
+                  <ControlledFormPage />
+                </Route>
+                <Route path="/forms/uncontrolled">
+                  <UncontrolledFormPage />
+                </Route>
+              </Switch>
+            </Router>
+          </Route>
+          <Route>
+            <NotFoundPage />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const NavBar = () => (
+    <ul>
+        <li>
+            <Link to="/">Home</Link>
+        </li>
+        <li>
+            <Link to="/counter?startingValue=2">Counter</Link>
+        </li>
+        <li>
+            <Link to="/people-list">People List</Link>
+        </li>
+        <li>
+            <Link to="/protected">Protected</Link>
+        </li>
+        <li>
+            <Link to="/forms">Forms</Link>
+        </li>
+    </ul>
+)
