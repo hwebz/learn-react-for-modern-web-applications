b2e8fda8a4acabd996c1884b3eadf5627c0c9daa
Do Manh Ha <ex-domanhha001@ocft.com>
2020-08-12 11:48:44 +0700

    NotFoundPage implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { HomePage, CounterButtonPage, PeopleListPage } from './pages'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { HomePage, CounterButtonPage, PeopleListPage, NotFoundPage } from './pages'
 
 function App() {
   return (
@@ -8,15 +8,20 @@ function App() {
       <Link to="/">Home</Link>
       <Link to="/counter?startingValue=2">Counter</Link>
       <Link to="/people-list">People List</Link>
-      <Route path="/" exact>
-        <HomePage />
-      </Route>
-      <Route path="/counter">
-        <CounterButtonPage />
-      </Route>
-      <Route path="/people-list">
-        <PeopleListPage />
-      </Route>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/counter">
+          <CounterButtonPage />
+        </Route>
+        <Route path="/people-list">
+          <PeopleListPage />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
     </Router>
   );
 }
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const NotFoundPage = () => (
+    <h4>Page not found</h4>
+)
